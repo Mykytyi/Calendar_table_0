3cddf1b742cd8bdbf10d3f77d460a750d3d43305
Nikita <cherevaty.nik@gmail.com>
2019-05-26 12:20:13 +0300

    'fixed_code'

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function createCalendar(id, year, month) {
-  let week = ['пн','вт','ср','чт','пт','сб','вс'];
+  const week = ['пн','вт','ср','чт','пт','сб','вс'];
 
   let newDiv = document.createElement('div');
   let calend = document.body.insertBefore(newDiv, document.body.getElementsByTagName('script')[0]);
@@ -7,24 +7,26 @@ function createCalendar(id, year, month) {
   calend.setAttribute('id', id);
 
   let mainTable = calend.appendChild(document.createElement('table'));
+  let mainTHead = mainTable.appendChild(document.createElement('thead'));
   let mainTBody = mainTable.appendChild(document.createElement('tbody'));
 
   let day = 1;
 
-  let mon = month - 1;
-  let d = new Date(year, mon);
+  let choosenDate = new Date(year, month - 1);
   let daysInTheMonth = daysInMonth(month, year);
-  let startWeek = d.getDay() - 1; // начало недели 0пн-6вс
+  console.log(daysInTheMonth);
+  let startWeek = choosenDate.getDay() - 1;
+  if (startWeek === -1) startWeek = 6; // начало недели 0пн-6вс
+  console.log(startWeek);
 
+  let amountOfRows;
   let row;
-  for (let i = 0; i < 1; i++) {
-    row = mainTBody.appendChild(document.createElement('tr'))
-      for (let j = 0; j < 7; j++) {
-        row.appendChild(document.createElement('th')).innerHTML = week[j];
-      }
+  row = mainTHead.appendChild(document.createElement('tr'))
+  for (let j = 0; j < 7; j++) {
+    row.appendChild(document.createElement('th')).innerHTML = week[j];
   }
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 6; i++) {
     row = mainTBody.appendChild(document.createElement('tr'))
     for (let j = 0; j < 7; j++) {
       if(i === 0 && j < startWeek || day > daysInTheMonth) {
@@ -35,10 +37,13 @@ function createCalendar(id, year, month) {
       }
     }
   }
+  if (row.children[0].innerHTML === '') {
+    mainTBody.lastElementChild.remove();
+  }
 }
 
 function daysInMonth(month, year) {
-    return new Date(year, month, 0).getDate();
+  return new Date(year, month, 0).getDate();
 }
 
-createCalendar("cal", 2012, 9);
+createCalendar("cal", 2019, 5);
